11e8ca7ed879f23293d14efc396f7188dcb53833
Prince <chamtail@126.com>
2018-08-08 04:45:26 +0800

    stop job | modify url

--- src/main/java/fdu/controller/GeneratorController.java
+++ src/main/java/fdu/controller/GeneratorController.java
@@ -1,7 +1,7 @@
 package fdu.controller;
 
-import fdu.bean.generator.ScalaDriverGenerator;
 import fdu.bean.executor.ShellExecutor;
+import fdu.bean.generator.ScalaDriverGenerator;
 import fdu.service.OperationParserService;
 import fdu.service.operation.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,14 +15,14 @@ import java.io.IOException;
  * Created by slade on 2016/11/28.
  */
 @RestController
-public class generatorController {
+public class GeneratorController {
 
 	@Autowired
 	private OperationParserService operationParserService;
 	@Autowired
 	private ShellExecutor shellExecutor;
 
-	@RequestMapping(value = "/", method = RequestMethod.POST)
+	@RequestMapping(value = "/runjob", method = RequestMethod.POST)
 	public String generateDriver(String config, String masterip, String masterport,
 			ScalaDriverGenerator scalaDriverGenerator) throws IOException {
 		Operation op = operationParserService.parse(config);
@@ -31,4 +31,15 @@ public class generatorController {
 		String program = scalaDriverGenerator.generate(op.getId());
 		return shellExecutor.executeCommand(program, masterip, masterport);
 	}
+
+	@RequestMapping(value = "/stopjob", method = RequestMethod.POST)
+    public String stopSparkJob() throws IOException {
+        String[] cmd = {
+                "/bin/sh",
+                "-c",
+                "jps | grep SparkSubmit | cut -d ' ' -f1 | xargs kill -9"
+        };
+        Runtime.getRuntime().exec(cmd);
+        return "ok";
+    }
 }
